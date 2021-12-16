edd9eaef1c975d71e5cb0f532186ed78497806f4
Bob <bob@example.com>
2021-12-16 10:09:07 -0300

    css emoji

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,8 @@ import linkedin from "../img/linkedin.png";
 import git from "../img/git.png";
 import Projects from './Projects';
 import hola from "../img/hola.PNG";
-import sobremi from '../img/mi.PNG'
+import sobremi from '../img/mi.PNG';
+import contact from '../img/contact.png'
 
 function Home() {
     
@@ -22,7 +23,7 @@ function Home() {
     
     return (
         <div className={style.container}>
-          <Nav className={style.nav}/> 
+          <Nav className={style.nav} /> 
          <section
          id='home'
          className={style.home}>
@@ -31,12 +32,12 @@ function Home() {
               <p> Hola! Bienvenid@ a mi Portfolio! Mi nombre es Santiago Prario y soy Full Stack Developer. Me forme en el bootcamp de Henry, el cual requiere mas de 800 horas de codigo para aprobarlo. Aqui estan expuestos algunos de mis proyectos del curso o de proyectos propios que he realizado para adquirir mas experiencia.
               </p>
           </div>
-          <img src={hola} alt="hola" height='400px' width='400px' /> 
+          <img src={hola} alt="hola"  /> 
          </section>
          <section
          id='sobremi'
          className={style.about}>
-             <img src={sobremi} alt="hola" height='400px' width='400px' /> 
+             <img src={sobremi} alt="hola"  /> 
              <div className={style.sobremi}>
              <h2>Sobre Mi</h2>
              <p>{aboutOne} </p>
@@ -54,6 +55,8 @@ function Home() {
          className={style.contact}>
          <h1>Contactarme</h1>
          <div className={style.iconsContact}>
+         <img  className={style.contactImg} src={contact} alt="/santiagoprario" height='150px' width='150px'  />
+
              <img src={linkedin} alt="/santiagoprario" height='70px' width='70px'  />
              <img src={mail} alt="santiagoprario@gmail.com" height='70px' width='70px' />
              <img src={whats} alt="+5492236846451" height='70px' width='70px' />
